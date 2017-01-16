459d890343bd82aaeed2e06df4896e1a750f633e
wotakuro <wotakuro@gmail.com>
2017-01-17 01:05:58 +0900

    commit

--- Assets/Editor/TextureAssetCreate.cs
+++ Assets/Editor/TextureAssetCreate.cs
@@ -24,14 +24,32 @@ public class TextureAssetCreate {
 
 		foreach (var path in pathList) {
 			Texture2D texture = AssetDatabase.LoadAssetAtPath<Texture2D> (path);
-			string outputPath = Path.Combine( outputDir,path );
+            byte[] rawData = texture.GetRawTextureData();
+            string outputPath = Path.Combine(outputDir, path) + ".bin";
 
 			FileStream fs = File.OpenWrite (outputPath);
+            fs.Write(FastTextureHeader, 0, FastTextureHeader.Length);
+            fs.Write(GetByteArrayFromInt(texture.width), 0, 4);
+            fs.Write(GetByteArrayFromInt(texture.height), 0, 4);
+            fs.Write(GetByteArrayFromInt((int)texture.format), 0, 4);
+            fs.Write(GetByteArrayFromInt((int)0), 0, 4); // todo flag
+
+            fs.Write(GetByteArrayFromInt(rawData.Length), 0, 4);
+            fs.Write(GetByteArrayFromInt(rawData.Length), 0, 4);
+            fs.Write(rawData, 0, rawData.Length);
+            fs.Write(GetByteArrayFromInt((int)0), 0, 4); // todo crc32
+
 			fs.Close ();
 		}
 	}
 
-	private readonly byte[] FastTextureHeader = new byte[]{};
+	private readonly byte[] FastTextureHeader = new byte[]{
+        0x46,0x53,0x54,0x54,0x45,0x58,0x00,0x00,
+    };
+
+    private readonly byte[] FastDatarArchiveHeader = new byte[]{
+        0x46,0x53,0x54,0x41,0x52,0x43,0x00,0x00
+    };
 
 	private byte[] GetByteArrayFromInt(int val){
 		byte[] ret = new byte[4];
@@ -50,8 +68,8 @@ public class TextureAssetCreate {
 			}
 		}
 		foreach (string createDir in directorySet) {
-			string targetPath = Path.Combine (outputDir, createDir);
-			if (Directory.Exists (targetPath)) {
+			string targetPath = Path.Combine( outputDir ,  createDir );
+			if (!Directory.Exists (targetPath)) {
 				Directory.CreateDirectory (targetPath);
 			}
 		}
---
+++ Assets/Samples/TestLoadComponent.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestLoadComponent : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+
+    public void TestLoad()
+    {
+
+    }
+}
