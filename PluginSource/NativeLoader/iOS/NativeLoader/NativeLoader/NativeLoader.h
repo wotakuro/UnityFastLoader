a8db6e345e61d88eeef07684df31e20e678a4597
YusukeKurokawa <ykurokawa@YusukeKurokawa-no-MacBook-Pro.local>
2017-02-01 23:09:08 +0900

    delete file

---
+++ PluginSource/NativeLoader/iOS/NativeLoader/NativeLoader/NativeLoader.h
@@ -0,0 +1,13 @@
+//
+//  NativeLoader.h
+//  NativeLoader
+//
+//  Created by YusukeKurokawa on 2017/02/01.
+//  Copyright © 2017年 YusukeKurokawa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NativeLoader : NSObject
+
+@end
